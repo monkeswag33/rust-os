dfa409967c132b565152d4b31463254fc0f95776
Ishan Karmakar <ishan.karmakar24@gmail.com>
2022-04-11 17:54:22 +0000

    Add file for printing text

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // Print Hello World to screen
-    let vga_buffer = 0xb8000 as *mut u8;
-    let color: u8 = 0xc;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = color;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
 
